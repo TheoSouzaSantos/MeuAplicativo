81e0f3df54561a4753a667e2deba1e01217a0e70
Théo Souza <ssztheo@gmail.com>
2026-08-31 15:42:29 -0300

    MeuAplicativo

--- app/src/main/java/com/example/aula1/MainActivity.kt
+++ app/src/main/java/com/example/aula1/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +23,62 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        val itens = mutableListOf<String>()
+
+        val listaPessoas = findViewById<ListView>(R.id.listaPessoas)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, itens)
+        listaPessoas.adapter = adapter
+
+        val pessoaescrita = findViewById<TextInputEditText>(R.id.pessoa)
+
+        val adicionarPessoa = findViewById<Button>(R.id.adicionarPessoa)
+        val removerPessoa = findViewById<Button>(R.id.removerPessoa)
+
+        adicionarPessoa.setOnClickListener {
+
+            val pessoa: String = pessoaescrita.text.toString()
+            if(itens.isEmpty() && pessoa.trim() == ""){
+
+            }
+            itens.add(pessoa.trim())
+            adapter.notifyDataSetChanged()
+            pessoaescrita.text?.clear()
+        }
+
+        removerPessoa.setOnClickListener {
+
+            val pessoa: String = pessoaescrita.text.toString()
+            if(itens.isNotEmpty()) {
+                if (pessoa.trim() != "") {
+                    if(itens.remove(pessoa.trim())){
+                        itens.remove(pessoa.trim())
+                    }else {
+                        Toast.makeText(
+                            this,
+                            R.string.txt_pessoanaoexiste,
+                            Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    itens.removeAt(itens.size - 1)
+                }
+            } else {
+                Toast.makeText(
+                    this,
+                    R.string.txt_listavaziaR,
+                    Toast.LENGTH_SHORT).show()
+            }
+            adapter.notifyDataSetChanged()
+            pessoaescrita.text?.clear()
+        }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
         menuInflater.inflate(R.menu.menu_principal, menu)
         return true
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -33,18 +87,20 @@ class MainActivity : AppCompatActivity() {
             R.id.item_configuracao -> {
                 Toast.makeText(
                     this@MainActivity,
-                    "Item Configuração selecionado",
+                    R.string.txt_addconfig,
                     Toast.LENGTH_LONG
                 ).show()
             }
             R.id.item_sobre -> {
                 Toast.makeText(
                     this@MainActivity,
-                    "Item Sobre selecionado",
+                    R.string.txt_addsobre,
                     Toast.LENGTH_LONG
                 ).show()
             }
         }
         return super.onOptionsItemSelected(item)
     }
+
+
 }
